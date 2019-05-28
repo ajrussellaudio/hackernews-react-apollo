aed3a3e82e945d56fac752543a3fe4843c53d544
Alan Russell <ajrussellaudio@gmail.com>
2019-05-28 10:38:18 +0100

    mutations: creating links

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
   return (
     <div className="App">
       <LinkList />
+      <CreateLink />
     </div>
   );
 }
